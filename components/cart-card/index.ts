d1a2f2bad61070d25f2bd51106a89e7f3516bfeb
SylllaR <54371465+SylllaR@users.noreply.github.com>
2023-02-16 10:05:03 +0200

    1 file modified, 3 added

---
+++ components/cart-card/index.ts
@@ -0,0 +1,3 @@
+import { CartCard } from './component';
+
+export default CartCard;
--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,4 @@ export { default as Card } from './card';
 export { default as ModalCard } from './modal-card';
 export { default as LangSelector } from './lang-selector';
 export { default as CartModal } from './cart-modal';
+export { default as CartCard } from './cart-card';
